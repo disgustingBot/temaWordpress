59539d18d3ee476a249f88236a018d7087b728df
molinerozadkiel <50532987+molinerozadkiel@users.noreply.github.com>
2019-10-22 11:32:42 -0300

    social sharing buttons conected (not instagram nor youtube)

--- functions.php
+++ functions.php
@@ -16,8 +16,6 @@ function en_el_init(){
 	);
 }
 
-
-
 add_action( 'init', 'en_el_init' );
 
 
@@ -175,3 +173,54 @@ function excerpt($charNumber){
 //
 //   return $args;
 // }
+
+
+
+
+
+
+// Function to handle the thumbnail request
+function get_the_post_thumbnail_src($img)
+{
+  return (preg_match('~\bsrc="([^"]++)"~', $img, $matches)) ? $matches[1] : '';
+}
+function wpvkp_social_buttons($content) {
+    global $post;
+    if(is_singular() || is_home()){
+
+        // Get current page URL
+        $sb_url = urlencode(get_permalink());
+
+        // Get current page title
+        $sb_title = str_replace( ' ', '%20', get_the_title());
+
+        // Get Post Thumbnail for pinterest
+        $sb_thumb = get_the_post_thumbnail_src(get_the_post_thumbnail());
+
+        // Construct sharing URL without using any script
+        $twitterURL = 'https://twitter.com/intent/tweet?text='.$sb_title.'&amp;url='.$sb_url.'&amp;via=wpvkp';
+        $facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$sb_url;
+        $linkedInURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$sb_url.'&amp;title='.$sb_title;
+
+
+        // Add sharing button at the end of page/page content
+        $content .= '<div class="social-box"><div class="social-btn">';
+        $content .= '<a class="col-1 sbtn s-twitter" href="'. $twitterURL .'" target="_blank" rel="nofollow"><span>Share on twitter</span></a>';
+        $content .= '<a class="col-1 sbtn s-facebook" href="'.$facebookURL.'" target="_blank" rel="nofollow"><span>Share on facebook</span></a>';
+        $content .= '<a class="col-2 sbtn s-linkedin" href="'.$linkedInURL.'" target="_blank" rel="nofollow"><span>LinkedIn</span></a>';
+        $content .= '</div></div>';
+
+        return $content;
+    }else{
+        // if not a post/page then don't include sharing button
+        return $content;
+    }
+};
+// Enable the_content if you want to automatically show social buttons below your post.
+
+ add_filter( 'the_content', 'wpvkp_social_buttons');
+
+// This will create a wordpress shortcode [social].
+// Please it in any widget and social buttons appear their.
+// You will need to enabled shortcode execution in widgets.
+add_shortcode('social','wpvkp_social_buttons');
